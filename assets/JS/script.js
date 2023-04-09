d73006f142ed8ec753c04452d7ddf763ea4d3728
Elena Madalina Gogosoiu <mangowings89@gmail.com>
2023-04-09 21:00:56 +0000

    Add functions keep track wrong letters, update correct letters, add event listener fired when a key pressed and reacts differently depending on selected word

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,5 +15,79 @@ const foundWords = [];
 const notfoundWords = [];
 
 
+//Show hidden word
+
+function displayWord(){
+    wordE1.innerHTML = `
+    ${selectedWord
+    .split('')
+    .map(
+        letter =>`
+        <span class="letter">
+        ${correctLetters.includes(letter) ? letter : ''}
+        </span>
+        `
+    )
+    .join('')}
+    `;
+
+    const innerWord = wordE1.innerText.replace(/\n/g, '');
+
+    if(innerWord === selecteWord){
+        finalMessage.innerText = 'Congratulation! You won!';
+        popup.style.display= 'flex';
+        incrementFound();
+    }
+}
+
+// Update the word letters
+function updateWrongletterE1(){
+    //Dispaly wrong letters
+    wrongLettersE1.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+    //Check if lost
+    if(wrongLetters.length === figureParts.length){
+        finalMessage.innerHTML = 'Unfortunaley you lost.';
+        popup.style.display = 'flex';
+        incrementNotfound();
+    }
+}
+
+//Show notification
+function showNotification(){
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e =>{
+    if(e.keyCode >= 65 && e.keyCode <= 90){
+        const letter = e.key;
+
+        if(selectedWord.includes(letter)){
+            if(!correctLetters.includes(letter)){
+                correctLetters.push(letter);
+
+                displayWord();
+            } else{
+                showNotification();
+            }
+        } else{
+            if(!wrongLetters.includes(letter)){
+                wrongLetters.push(letter);
+
+                updateWrongLettersE1();
+            } else{
+                showNotification();
+            }
+        }
+    }
+});
+
 
 
