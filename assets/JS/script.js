dd1de9436ca0f52ae044e6b67e28966a67d09b3f
Elena Madalina Gogosoiu <mangowings89@gmail.com>
2023-04-10 10:14:11 +0000

    Change the structure of HTML divs, add the rod and parts figure

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,7 +3,7 @@ const wrongLettersE1 = document.getElementById('wrong-letters');
 const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('pupup-container');
 const notification = document.getElementById('notification-container');
-const finalMESSAGE = document.getElementById('final-message');
+const finalmessage = document.getElementById('final-message');
 const words = ['aberrant', 'assiduous', 'restaurateur', 'corroborate', 'curriculum', 'defamation', 'deprivation', 'dissociate', 'espionage', 'exaggeration', 'diurnal', 'distinction', 'programming', 'practical', 'software', 'development', 'modernism', 'situationist', 'syndrome', 'jawbreaker'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
